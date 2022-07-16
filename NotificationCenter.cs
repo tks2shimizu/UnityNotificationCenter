7d543ee5a5017e5f2a5b15d9a1c76d49a91b66a2
Tomoaki Shimizu <tks2shimizu@users.noreply.github.com>
2022-07-16 17:42:26 +0900

    NotificationCenter.cs の更新

--- NotificationCenter.cs
+++ NotificationCenter.cs
@@ -1,38 +1,125 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
-public class TextController : MonoBehaviour
+public class NotificationCenter : MonoBehaviour
 {
-    [SerializeField] private Text countUpLabel;
-    
-    void Start()
+    private static NotificationCenter _defaultCenter;
+    public static NotificationCenter DefaultCenter
     {
-        countUpLabel = GetComponent<Text>();
+        get
+        {
+            if (!_defaultCenter)
+            {
+                var notificationObject = new GameObject("Default Notification Center");
 
-        NotificationCenter.DefaultCenter.AddObserver(this, nameof(CountUp));
-        NotificationCenter.DefaultCenter.AddObserver(this, nameof(CountDown));
+                _defaultCenter = notificationObject.AddComponent<NotificationCenter>();
+            }
+
+            return _defaultCenter;
+        }
     }
 
-    void CountUp(NotificationCenter.Notification notification)
-    {
-        foreach (DictionaryEntry data in notification.data)
+    private readonly Hashtable _notifications = new Hashtable();
+
+    public void AddObserver(Component observer, string name)
+    { 
+        if (string.IsNullOrEmpty(name))
         {
-            if ((string)data.Key == "count")
+            Debug.Log("Null name specified for notification in AddObserver.");
+            return;
+        }
+
+        _notifications[name] ??= new List<Component>();
+
+        if (_notifications[name] is List<Component> notifyList && !notifyList.Contains(observer))
+        {
+            notifyList.Add(observer);
+        }
+    }
+
+    public void RemoveObserver(Component observer, string name)
+    {
+        var notifyList = (List<Component>)_notifications[name];
+
+        if (notifyList != null)
+        {
+            if (notifyList.Contains(observer))
+            { 
+                notifyList.Remove(observer);
+            }
+
+            if (notifyList.Count == 0)
             {
-                countUpLabel.text = ((int)data.Value).ToString();
+                _notifications.Remove(name);
             }
         }
     }
 
-    void CountDown(NotificationCenter.Notification notification)
+    public void PostNotification(Component aSender, string aName)
     {
-        foreach (DictionaryEntry data in notification.data)
+        PostNotification(aSender, aName, null);
+    }
+
+    public void PostNotification(Component aSender, string aName, Hashtable aData)
+    {
+        PostNotification(new Notification(aSender, aName, aData));
+    }
+
+    public void PostNotification(Notification aNotification)
+    {
+        if (string.IsNullOrEmpty(aNotification.name))
         {
-            if ((string)data.Key == "count")
+            Debug.Log("Null name sent to PostNotification.");
+            return;
+        }
+
+        var notifyList = (List<Component>)_notifications[aNotification.name];
+
+        if (notifyList == null)
+        {
+            Debug.Log("Notify list not found in PostNotification: " + aNotification.name);
+            return;
+        }
+
+        var observersToRemove = new List<Component>();
+
+        foreach (var observer in notifyList)
+        {
+            if (!observer)
             {
-                countUpLabel.text = ((int)data.Value).ToString();
+                observersToRemove.Add(observer);
             }
+            else
+            {
+                observer.SendMessage(aNotification.name, aNotification, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+
+        foreach (var observer in observersToRemove)
+        {
+            notifyList.Remove(observer);
+        }
+    }
+
+    public class Notification
+    {
+        public Component sender;
+        public string name;
+        public Hashtable data;
+
+        public Notification(Component aSender, string aName)
+        {
+            sender = aSender; 
+            name = aName;
+            data = null;
+        }
+
+        public Notification(Component aSender, string aName, Hashtable aData)
+        {
+            sender = aSender;
+            name = aName;
+            data = aData;
         }
     }
 }
